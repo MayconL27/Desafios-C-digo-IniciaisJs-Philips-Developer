5694ee9f6bc9d2bce55917e74c18204c8973c18c
MayconL27 <mayconleandro2008@gmail.com>
2022-04-11 15:39:32 -0300

    Atualização

---
+++ Desafio02.js
@@ -0,0 +1,15 @@
+let lines = gets().split('\n');
+
+let line = lines.shift().split(" ");
+let inicio = parseInt(line[0]);
+let final = parseInt(line[1]);
+
+if (inicio >= 0 && final <= 2) {
+    print('nova');
+} else if (final > inicio && final <=     ) {
+    print(               );
+} else if (              ) {
+    print(                 );                 //complete o código nos espaços em branco
+} else { 
+    print('cheia');
+}
